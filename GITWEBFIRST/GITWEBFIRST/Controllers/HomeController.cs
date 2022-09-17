5d3559c3703bd35a8b03c68b69be89f2112fe865
Subash Sharma <sharmasubash955@gmail.com>
2022-09-17 20:26:41 +0530

    Adding MVC into project For this added services.addcontollerswithviews() in startup.cs also added default contoller route by using endpoints.MapdefaultcontrollerBase() Created Controller folder and homeController file

---
+++ GITWEBFIRST/GITWEBFIRST/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GITWEBFIRST.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index()
+        {
+            return "Method data type is string and return type should be string";
+        }
+    }
+}
--- GITWEBFIRST/GITWEBFIRST/Startup.cs
+++ GITWEBFIRST/GITWEBFIRST/Startup.cs
@@ -16,6 +16,7 @@ namespace GITWEBFIRST
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +31,19 @@ namespace GITWEBFIRST
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    if (env.IsDevelopment())
+                //    {
+                //        await context.Response.WriteAsync("Hello from development!");
+
+                //    }
+
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
+            
         }
     }
 }
